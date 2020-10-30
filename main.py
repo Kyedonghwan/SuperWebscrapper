0dbcc6c7e72bdaab6607bbd115fe49d31b088419
kyedonghwan-linux <kdh643737@gmail.com>
2020-10-30 12:21:10 +0900

    search

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from flask import Flask, render_template, request
+
+
+app = Flask("SuperScrapper")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html", searching_by=word)
+
+
+app.run()
+
+
+"""
 from indeed import get_jobs as get_indeed_jobs
 from stackoverflow import get_jobs as get_stackoverflow_jobs
 from save import save_to_file
@@ -8,3 +29,4 @@ indeed_jobs = get_indeed_jobs()
 
 jobs = indeed_jobs + stackoverflow_jobs
 save_to_file(jobs)
+"""
